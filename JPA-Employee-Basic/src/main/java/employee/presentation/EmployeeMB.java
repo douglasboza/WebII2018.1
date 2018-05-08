75f4460da9f9a5d315a5d2f299abb4268bd93d19
douglasboza <douglasboza@gmail.com>
2018-05-08 20:12:42 -0300

    add employer

---
+++ JPA-Employee-Basic/src/main/java/employee/presentation/EmployeeMB.java
@@ -0,0 +1,48 @@
+package employee.presentation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import employee.Employee;
+import employee.data.EmployeeDAO;
+
+@Named(value = "employeeManagedBean")
+@RequestScoped
+public class EmployeeMB {
+	@Inject EmployeeDAO dao;
+	
+	//Auxiliary fields for JSF
+	private List<Employee> employeeList = new ArrayList<>();
+	private Employee employee = new Employee();
+	
+        
+        
+	public List<Employee> getEmployeeList() {
+                employeeList = dao.findEmployees();
+                System.out.println( "teste okok" );
+		return employeeList;
+	}
+
+	public void setEmployeeList(List<Employee> employeeList) {
+		this.employeeList = employeeList;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public String addNewEmployee() {
+		dao.addNew(employee);
+		employeeList = dao.findEmployees();
+		return "employeelist";
+	}
+	
+}
